9a0a6e3e667b10425119961ac64614334d2d79fc
jfranguerrero <jfranguerrero@gmail.com>
2016-10-26 21:20:00 +0200

    Añadido fichero de tests y sus dependencias closes jfranguerrero/IV#10

---
+++ vuelaBot/test_vuelabot.py
@@ -0,0 +1,56 @@
+import unittest
+from requests import HTTPError
+from skyscanner.skyscanner import Flights
+import modules
+import os
+import time
+import pickle
+import sqlite3
+
+
+def load_obj(name ):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+TOKEN =os.environ['token_vuelabot']
+
+
+
+class vuelaBotTest(unittest.TestCase):
+    def test_busqueda_vuelo(self):
+        self.result=modules.buscarvuelo_live('AGP', 'BCN', '2016-12-10', '2016-12-15')
+        self.assertTrue(self.result is not None)
+        self.assertTrue('Itineraries' in self.result)
+        self.assertTrue(len(self.result['Itineraries']) > 0)
+
+    def test_procesamiento_mensaje(self):
+        salida=modules.separar_mensaje_busqueda('AGPBCN12/11/201623/12/2016')
+        self.assertEquals(salida['origen'],'AGP')
+        self.assertEquals(salida['destino'],'BCN')
+        self.assertEquals(salida['fida'],'12/11/2016')
+        self.assertEquals(salida['fvuelta'],'23/12/2016')
+
+    def test_conversion_fecha(self):
+        salida=modules.convertir_fechas('12/11/2016','23/12/2016')
+        self.assertEquals(salida['fida'],'2016-11-12')
+        self.assertEquals(salida['fvuelta'],'2016-12-23')
+
+
+    def test_procesamiento_resultados(self):
+        result_tests=load_obj('vuelos_test_results')
+        salida=modules.procesar_resultados(result_tests)
+        results_procesamiento=load_obj('test_resultado_mostrar')
+        self.assertEquals(salida,results_procesamiento)
+
+
+    def test_basedatos(self):
+        con_bd = sqlite3.connect('vuelabot.db')
+        c = con_bd.cursor()
+        ciudad=('BCN', )
+        c.execute('SELECT cod_aeropuerto FROM aeropuertos WHERE cod_aeropuerto=?', ciudad)
+        self.assertEquals(c.fetchone()[0],'BCN')
+        con_bd.close()
+
+if __name__ == '__main__':
+    unittest.main()
