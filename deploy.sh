8a45ff0e16cb8167ef144931c07a46545f01305e
jfranguerrero <jfranguerrero@gmail.com>
2017-01-29 04:00:17 +0100

    Añadido script de despliegue closes jfranguerrero/IV#24

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+
+wget https://releases.hashicorp.com/vagrant/1.8.7/
+sudo dpkg -i vagrant_1.8.7_x86_64.deb
+
+sudo vagrant plugin install vagrant-azure
+
+# Instalación Ansible
+sudo apt-get install ansible
+
+
+# Despliegue en Azure
+sudo vagrant up --provider=azure
+
+# Despliegue de la aplicación con Fabric
+sudo pip install fabric
+# Actualiza el supervisor
+fab -p N1_n2n3n4_N5 -H usuario@vuelabot.cloudapp.net nohup
