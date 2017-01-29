8dce3574219c8a61832cd3d14d153eb1ad5c1fd1
jfranguerrero <jfranguerrero@gmail.com>
2017-01-29 03:58:04 +0100

    Corrección de ficheros

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+import os
 
 env.host = ['usuario:22']
 
@@ -6,8 +7,13 @@ def download():
     run ('sudo rm -rf IV')
     run ('sudo git clone https://github.com/jfranguerrero/IV')
 
+def nohup():
+    with shell_env(api_skyscanner=os.environ['api_skyscanner'], DATABASE_URL=os.environ['DATABASE_URL'], token_vuelabot=os.environ['token_vuelabot']):
+        run("nohup python IV/vuelaBot/vuelabot.py >& /dev/null < /dev/null &",pty=False)
+
 def start():
-    run ('sudo python IV/vuelaBot/vuelabot.py')
+    with shell_env(api_skyscanner=os.environ['api_skyscanner'], DATABASE_URL=os.environ['DATABASE_URL'], token_vuelabot=os.environ['token_vuelabot']):
+        run ('python IV/vuelaBot/vuelabot.py')
 
 def stop():
     run ('kill -9 $(pidof python)')
@@ -19,4 +25,4 @@ def tests():
     run ('cd IV && make test')
 
 def install():
-run ('cd IV && make install')
+    run ('cd IV && make install')
